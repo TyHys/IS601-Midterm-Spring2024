ca7fdd73e15983ab6b36f50f15d79d6a661f320a
TyHys <tyler.hayes@live.com>
2024-03-24 15:56:18 -0400

    Added tests for exit command

--- commands/exit.py
+++ commands/exit.py
@@ -1,18 +1,28 @@
+# pylint: disable=too-few-public-methods
+"""
+A command class that implements the exit command.
+"""
+import sys
 class Command:
+    """
+    A command class that implements the exit command.
+    """
     def execute(self, calculator):
         """
         Executes the exit command, terminating the application.
 
-        This method terminates the application when called. It prints a message indicating that the exit command
-        has been received and ends the execution of the program. Any cleanup operations needed before termination
+        This method terminates the application when called. 
+        It prints a message indicating that the exit command
+        has been received and ends the execution of the program. 
+        Any cleanup operations needed before termination
         should be performed before calling this method.
 
         Parameters:
-        calculator (Calculator): The calculator instance. This parameter is not used in this method but is required
-        by the interface for consistency with other command classes.
+        calculator (Calculator): Defines the calculator object
+        which will have its history exported.
         """
+        calculator.calc_hist.export_history()
         print("Exit command received; ending application")
         # You can perform any cleanup operations here if needed
         # For example, closing files, releasing resources, etc.
-        import sys
         sys.exit()  # Exit the application
---
+++ tests/test_cmd_exit.py
@@ -0,0 +1,25 @@
+# pylint: disable=too-few-public-methods
+"""
+Unit test for the exit command
+"""
+from unittest.mock import MagicMock
+import pytest
+from commands.exit import Command
+
+class TestCommand:
+    """
+    TestCommand class for the exit command
+    """
+    def test_execute(self):
+        """
+        Test that the execute method returns the expected value.
+        """
+        calculator = MagicMock()
+        calculator.calc_hist = MagicMock()
+
+        command = Command()
+        with pytest.raises(SystemExit) as pytest_wrapped_e:
+            command.execute(calculator)
+
+        assert pytest_wrapped_e.type == SystemExit
+        assert pytest_wrapped_e.value.code is None
